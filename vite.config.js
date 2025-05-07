bbcaf5da3415301280c6e34d1af102990b960953
Efren Muñoz Alvarez <efr.munoz@duocuc.cl>
2025-05-07 11:18:40 -0400

    Actualización
    Subida de archivos para despliegue Git

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portafolio-efrenmunozalvarezTesting/', // 👈 nombre exacto del repo
   plugins: [
     tailwindcss(),
     react()
